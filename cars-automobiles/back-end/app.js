7aa4abe9b79163d4adbbafc4c763417fc5daffa7
Srikrishna Sasidharan <sr950863@dal.ca>
2020-08-04 16:35:09 -0300

    Sequelize ORM for cars-inventory

--- cars-automobiles/back-end/app.js
+++ cars-automobiles/back-end/app.js
@@ -1,4 +1,65 @@
 var Sequelize = require('sequelize');
+const express = require('express')
+const app = express()
+const port = 3001;
+app.use(express.json());
+var sequelize = new Sequelize('cars-automobiles-team18', 'myuser@a6team18', 'qwerty@1234', {
+    host: 'a6team18.mysql.database.azure.com',
+    dialect: 'mysql',
+    port: 3306,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    });
+    var Cars = sequelize.define('cars', {
 
-var sequelize = new Sequelize('mysql://a6team18.mysql.database.azure.com:3306/database', 'username', 'password')
+        car_name: 
+        {
+         type: Sequelize.STRING,
+         allowNull: false   
+         },
+        car_id:
+        {
+         type: Sequelize.INTEGER,
+         allowNull: false,
+         primaryKey: true
+        }, 
+        model:
+        {
+            type: Sequelize.STRING,
+            allowNull: false
+           },
+           createdAt:{
+               type: Sequelize.TIME
+           },
+           
+           updatedAt:{
+               type: Sequelize.TIME
+           }
+    }, {  tableName: 'cars'
+}); 
+
+app.listen(port, '0.0.0.0', () => console.log(`Company X app listening on port ${port}!`))
+
+app.get('/getAllCars', (req, res) => {
+   
+    Cars.findOne({'car_name':'audi'}).then(function (user,err) {
+        if(err){
+            throw err;
+        }
+        console.log(user);
+       
+    }).catch(error => { console.log('caught', error.message); });;
+
+    Cars.findAll({}).then(function (user,err) {
+        if(err){
+            throw err;
+        }
+        console.log(user);
+       
+    }).catch(error => { console.log('caught', error.message); });;
+});
+module.exports = app;
 
