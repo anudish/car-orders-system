a8e5ad59f1ebbeab77287a4c848e70f7866996c2
Srikrishna Sasidharan <sr950863@dal.ca>
2020-08-07 02:31:59 -0300

    p\order hisory

--- cars-automobiles/carautomobiles-frontend/src/app/app-routing.module.ts
+++ cars-automobiles/carautomobiles-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddCarsComponent } from './add-cars/add-cars.component'
 import { ListCarsComponent } from './list-cars/list-cars.component'
+import { OrderhistoryComponent } from './orderhistory/orderhistory.component';
 
 const routes: Routes = [
   { path: 'addcar', component: AddCarsComponent },
   { path: '', component: ListCarsComponent },
   { path: 'updatecar', component: AddCarsComponent },
+  {path:'orderhistory', component: OrderhistoryComponent}
 ];
 
 @NgModule({
